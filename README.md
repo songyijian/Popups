accdd152365fef5c0a085db3511b36150ebccc35
songyijian <397032724@qq.com>
2020-08-18 00:51:30 +0800

    重构基本完成

--- README.md
+++ README.md
@@ -1,52 +1,41 @@
 # BombBox 1.1.0 _轻量级弹窗组件（支持amd）  
 
 ### 更新说明：
-* 1.1.0优化居中方案，
-* 默认css3入场动画（.css），
-* 增加initStatus:true 默认初始化显示弹窗， 
-* 增加form:stick（狗皮膏药模式）关闭隐藏窗口不删除现有dom
-* 1.1.1 {t} 支持dom对象,为了适应vue插槽
-* 1.1.1 bombClass="" 同时作用背景
+精简代码，只保留核心功能，用es6 class重构
+
 
 
 ## 函数 API
- 
-### 配置
-```
-new BombBox(
-    t,  //{string | dom} t string弹框内容（`<p>xxx</p>` | <p>xxx</p>）
-    {
-        'initStatus':true,              //初始化显示状态 默认显示弹窗
-        'form':'',                      //默认关闭删除dom， stick只隐藏窗口
-        'type':'',                      //弹框类型  alert,  confirm , prompt , textarea
-        'bombClass':'',                 // bomb-document增加class，同时作用背景
-        'center':true,                  //中间位置 (mr)
-        'bg':true,                      //是否有背景(mr)
-        'closeHtml':false,              //关闭按钮的内容
-        'timeOut':0,                    //定时关闭
-        "textareaValue":"",             //textarea = value
-        'promptValue':"",               //prompt=Value
-        'initFn':function(_this){},     //初始化回调
-        'closeFn': function(_this){},   //关闭回调
-        'yesBtnFn':function(value){},   //点击确定按钮回调·
-        'append': document.body         //插入位置
-    }
-)
-```
-
-### FN
-```
-this.html('data',fn(This))  //修改内容区域（信息,回调）
-this.center()               //居中方法 （1.1.0 已经没有意义）
-this.close('stick')         //关闭, 可以传参（‘stick’）模式，不传默认是删除dom
-this.show()                 //显示隐藏状态下的弹窗，(stick模式,initStatus:false)
 
 ```
 
-### ATTR
+API
+    new BombBox(
+      contentString,  // 弹框内容 innerHTML = htmlstring（ `<p>xxx</p>`）
+      {
+        'initshow': true, //初始化显示状态 默认显示弹窗
+        'bombClass': '', // 增加一个class
+        'bg': true, //是否有背景
+        'closeHtml': false, //关闭按钮的内容
+        'timeOut': 0, //定时关闭
+        'initFn': function (_this) {}, //初始回调
+        'closeFn': function (_this) {}, //关闭回调
+        'creact': function (_this) {}, //关闭回调
+        'remove': function (_this) {}, //关闭回调
+        'append': document.body //插入位置
+      }
+    )
+
+
+FN
+    this.updateHtml('data', fn(This)) //修改内容区域（信息,回调）
+    this.close()                    //关闭
+    this.show()                 //显示隐藏状态下的弹窗，(stick模式,initStatus:false)
+
+ATTR
+    this.live // 0=dom不存在 | 1=创建并插入了指定位置
 ```
-this.disStatus  //组件状态 -1_DOM不存在 0虚拟dom还没插入渲染  1_DOM存在处于隐藏状态 2_DOM存在并显示
-```
+
 
 
 ## 弹框DOM结构：
@@ -62,68 +51,3 @@ this.disStatus  //组件状态 -1_DOM不存在 0虚拟dom还没插入渲染  1_D
     </div>
 ```
 
-### alert
-```
-背景
-    <div class="bomb_bg"></div>
-
-弹框结构：
-    <div class="alert_document">
-        <button class="bomb_yes_btn">确定</button>
-        <big class="bomb_closebtn"></big>
-        <div class="bomb_html">
-            <!-- 允许在这里插入内容 -->
-        </div>
-    </div>
-```
-
-
-### confirm
-```
-背景
-    <div class="bomb_bg"></div>
-
-弹框结构：
-    <div class="confirm_document">
-        <button class="bomb_no_btn">取消</button>
-        <button class="bomb_yes_btn">确定</button>
-        <big class="bomb_closebtn"></big>
-        <div class="bomb_html">
-                <!-- 允许在这里插入内容 -->
-        </div>
-    </div>
-```
-
-### prompt
-```
-背景
-    <div class="bomb_bg"></div>
-
-弹框结构：
-    <div class="prompt_document">
-        <button class="bomb_no_btn">取消</button>
-        <button class="bomb_yes_btn">确定</button>
-        <big class="bomb_closebtn"></big>
-        <div class="bomb_html">
-                <!-- 允许在这里插入内容 -->
-            <input class="prompt_input" type="text">
-        </div>
-    </div>
-```
-
-### textarea
-```
-背景
-    <div class="bomb_bg"></div>
-
-弹框结构：
-    <div class="textarea_document">
-        <button class="bomb_no_btn">取消</button>
-        <button class="bomb_yes_btn">确定</button>
-        <big class="bomb_closebtn"></big>
-        <div class="bomb_html">
-                <!-- 允许在这里插入内容 -->
-            <textarea class="textarea_input"></textarea>
-        </div>
-    </div>
-```
--- src/BombBox.2.0.0.js
+++ src/BombBox.2.0.0.js
@@ -1,42 +1,35 @@
 ﻿/*
-    BombBox 1.1.0 _轻量级弹窗组件（支持AMD、链式写法）
-    作者：songyijian 
-    发布：2017.6.6 
-    github：https://github.com/songyijian/BombBox
+  BombBox 2.0.0 _轻量级弹窗组件（支持AMD、链式写法）
+  作者：songyijian 
+  发布：2020.8.18 
+  github：https://github.com/songyijian/BombBox
 
-    更新说明：1.1.0优化居中方案，
-          默认css3入场动画（.css），
-          增加initStatus:true 默认初始化显示弹窗， 
-          增加form:stick（狗皮膏药模式）关闭隐藏窗口不删除现有dom
+  更新说明：精简代码，只保留核心功能，用es6 class重构
 
 
-    API
-            t，string弹框内容（`<p>xxx</p>`）
-        new BombBox(t,{
-            'initStatus':true,              //初始化显示状态 默认显示弹窗
-            'form':'',                      //默认关闭删除dom， stick只隐藏窗口
-            'type':'',                      //弹框类型  alert,  confirm , prompt , textarea
-            'bombClass':'',                 // div class="bomb-document 这里增加一个class" >
-            'center':true,                  //中间位置 (mr)
-            'bg':true,                      //是否有背景(mr)
-            'closeHtml':false,              //关闭按钮的内容
-            'timeOut':0,                    //定时关闭
-            "textareaValue":"",             //textarea = value
-            'promptValue':"",               //prompt=Value
-            'initFn':function(_this){},     //初始化回调
-            'closeFn': function(_this){},   //关闭回调
-            'yesBtnFn':function(value){},   //点击确定按钮回调·
-            'append': document.body         //插入位置
-        })
-        
-        FN
-            this.html('data',fn(This))  //修改内容区域（信息,回调）
-            this.center()               //居中方法 （1.1.0 已经没有意义）
-            this.close('stick')         //关闭, 可以传参（‘stick’）模式，不传默认是删除dom
-            this.show()                 //显示隐藏状态下的弹窗，(stick模式,initStatus:false)
+  API
+    new BombBox(
+      contentString, // 弹框内容 innerHTML = htmlstring（ `<p>xxx</p>`）
+      {
+        'initShow': true, //初始化显示状态 默认显示弹窗
+        'bombClass': '', // 增加一个class
+        'bg': true, //是否有背景
+        'closeHtml': false, //关闭按钮的内容
+        'timeOut': 0, //定时关闭
+        'creact': function (_this) {}, //创建回调（未插入）
+        'show': function (_this) {}, //显示回调
+        'close': function (_this) {}, //关闭回调（从插入位置删除，但内存中存在）
+        'append': document.body //插入位置
+      }
+    )
+    
+    FN
+      this.updateHtml('data', fn(This)) //修改内容区域（信息,回调）
+      this.close()                    //关闭
+      this.show()                 //显示隐藏状态下的弹窗，(stick模式,initStatus:false)
 
-        ATTR
-            this.disStatus  //组件状态 0_DOM不存在 1_DOM存在处于隐藏状态 2_DOM存在并显示
+    ATTR
+      this.live // 0=dom不存在 | 1=创建并插入了指定位置
 */
 
 
@@ -48,30 +41,26 @@ class BombBox{
       return
     }
     this.t = contentString; //内容	
-    this.config = config
-    this.live= 0 ;   // 0=dom不存在创建 | 1=创建并插入了指定位置
-    // this.show = 0;    // 0 处于曝光状态
-
+    this.live = 0;   // 0 = dom不存在 | 1=创建并插入了指定位置
     this.config = Object.assign({ //配置参数
-      'initshow':true,                //初始化显示状态 默认显示弹窗
-      'bombClass':'',                 // div class="bomb-document 这里增加一个class
-      'bg':true,                      //是否有背景(mr)
-      'closeHtml':false,              //关闭按钮的内容
-      'timeOut':0,                    //定时关闭
-      'initFn': function(_this){},     //初始化后fun
-      'closeFn': function(_this){},   //关闭回调
-      'creact':function(_this){},   //关闭回调
-      'remove':function(_this){},   //关闭回调
-      'append': document.body         //插入位置
+      'initShow': true, //初始化显示状态 默认显示弹窗
+      'bombClass': '', // 增加一个class
+      'bg': true, //是否有背景
+      'closeHtml': false, //关闭按钮的内容
+      'timeOut': 0, //定时关闭
+      'creact': function (_this) {}, //创建回调（未插入）
+      'show': function (_this) {}, //显示回调
+      'close': function (_this) {}, //关闭回调（从插入位置删除，但内存中存在）
+      'append': document.body //插入位置
     },config);
 
-    console.log(this.config)
+
+    // console.log(this.config)
 
     this._init_();
   }
 
   _init_(){
-
     this.bombDocument = document.createElement('div');
     this.bombClose = document.createElement('big');
     this.bombHtml = document.createElement('div');
@@ -81,33 +70,36 @@ class BombBox{
     this.updateHtml(this.t)
 
     this.bombDocument.className = 'bomb_document '+ this.config.bombClass;
-
     this.bombDocument.appendChild(this.bombClose);
     this.bombDocument.appendChild(this.bombHtml);
 
     if (this.config.bg) {
       this.bombBg = document.createElement('div');
       this.bombBg.className = 'bomb_bg';
-      this.config.append.appendChild(this.bombBg);
     };
 
-    if(!this.config.initshow){
-      this.bombDocument.style.display="none";
-      this.bombBg && (this.bombBg.style.display="none");
-    }
+    this.bombClose.addEventListener('click',(params) => {
+      this.close()
+    },false)
 
-    if(this.config.initshow){//当前DOM状态
-      this.show()
-    }
+    this.config.creact(this)
+
+    this.config.initShow && this.show()
   }
 
   show () {
     if(this.live){ return }
     this.live = 1;
     this.config.append.appendChild(this.bombDocument);
-    this.config.bg && this.config.append.removeChild(this.bombBg); 
+    this.config.bg && this.config.append.appendChild(this.bombBg);
 
-    this.config.initFn(this)
+    if (this.config.timeOut > 0){
+      setTimeout(() => {
+        this.close()
+      }, this.config.timeOut);
+    }
+
+    this.config.show(this)
     return this;
   }
 
@@ -117,223 +109,22 @@ class BombBox{
       this.config.append.removeChild(this.bombDocument);
       this.config.bg && this.config.append.removeChild(this.bombBg);
       
-      this.config.closeFn(this)
+      this.config.close(this)
     }
     return this;
   };
 
 
-  updateHtml(t) { 
+  updateHtml(t,fn) { 
     this.t = t
     this.bombHtml.innerHTML = this.t; 
+    fn && fn(this)
     return this; 
   }
 
   html(t){
     this.updateHtml(t)
   }
-
 }
 
-
 Window.BombBox = BombBox
-
-
-
-
-
-
-// ! function() {
-//     'sue strict'
-//     function BombBox(t, key) {
-//         this.t = t; //内容	
-//         this.key = key
-//         this.disStatus=0;   //dom状态
-//         this.dataJ = { //配置参数
-//             'initStatus':true,              //初始化显示状态 默认显示弹窗
-//             'form':'',                      //默认关闭删除dom， stick只隐藏窗口
-//             'type':'',                      //弹框类型  alert,  confirm , prompt , textarea
-//             'bombClass':'',                 // div class="bomb-document 这里增加一个class" >
-//             'center':true,                  //中间位置 (mr)
-//             'bg':true,                      //是否有背景(mr)
-//             'closeHtml':false,              //关闭按钮的内容
-//             'timeOut':0,                    //定时关闭
-//             "textareaValue":"",             //textarea = value
-//             'promptValue':"",               //prompt=Value
-//             'initFn':function(_this){},     //初始化后fun
-//             'closeFn': function(_this){},   //关闭回调
-//             'yesBtnFn':function(value){},   //点击确定按钮回调
-//             'append': document.body         //插入位置
-//         };
-//         for (var i in key) { this.dataJ[i] = key[i] };
-//         this.init(this.dataJ.type);
-//         return this;
-//     };
-
-//     BombBox.prototype.init = function(type) {
-//         this.createDom(type)
-//         if (this.dataJ.center) { this.center() }; 
-
-//         //事件处理
-//         var _this = this;
-//         if (this.promptInput || this.bombYesBtn)
-//             this.promptInput ? this.promptInput.focus() : this.bombYesBtn.focus();
-
-//         if (type === "alert" || type === "confirm" || type === "prompt" || type === "textarea") {
-//             this.bombDocument.onkeyup = function(e) {
-//                 var keyCode = (window.event) ? e.which : e.keyCode;
-//                 if (keyCode === 13) {
-//                     _this.yesFn(this, type);
-//                 } else if (keyCode === 27) {
-//                     _this.close(_this.dataJ.form);
-//                 }
-//             }
-//         }
-//         if (this.bombClose)
-//             this.bombClose.onclick = function() { _this.close(_this.dataJ.form) };
-//         if (this.bombNoBtn)
-//             this.bombNoBtn.onclick = function() { _this.close(_this.dataJ.form) };
-//         if (this.bombYesBtn)
-//             this.bombYesBtn.onclick = function() { _this.yesFn(this, type) };
-//         if (_this.dataJ.timeOut) {
-//             setTimeout(function() {
-//                 _this.close(_this.dataJ.form);
-//             }, _this.dataJ.timeOut)
-//         }
-//         this.dataJ.initFn(this);
-//         return this;
-//     };
-
-//     BombBox.prototype.createDom = function(type) {
-//         this.bombDocument = document.createElement('div');
-//         this.bombClose = document.createElement('big');
-//         this.bombHtml = document.createElement('div');
-//         this.bombClose.className = 'bomb_closebtn';
-//         if (this.dataJ.closeHtml) { this.bombClose.innerHTML = this.dataJ.closeHtml; }
-//         this.bombHtml.className = 'bomb_html';
-//         this.bombHtml.innerHTML = this.t;
-//         if (!type) {
-//             this.bombDocument.className = 'bomb_document';
-//         } else if (type === "alert" || type === "confirm" || type === "prompt" || type === "textarea") {
-//             if (type === "alert") {
-//                 this.bombDocument.className = 'alert_document';
-//                 this.bombYesBtn = document.createElement('button');
-//                 this.bombYesBtn.className = "bomb_yes_btn"
-//                 this.bombYesBtn.innerHTML = "确定";
-//                 this.bombDocument.appendChild(this.bombYesBtn);
-//             } else if (type === "confirm" || type === "prompt" || type === "textarea") {
-//                 if (type === "confirm")
-//                     this.bombDocument.className = 'confirm_document';
-//                 if (type === "prompt" || type === "textarea")
-//                     type === "prompt" ? this.bombDocument.className = 'prompt_document' : this.bombDocument.className = 'textarea_document';
-//                 this.bombYesBtn = document.createElement('button');
-//                 this.bombYesBtn.className = "bomb_yes_btn"
-//                 this.bombYesBtn.innerHTML = "确定";
-//                 this.bombNoBtn = document.createElement('button');
-//                 this.bombNoBtn.className = "bomb_no_btn"
-//                 this.bombNoBtn.innerHTML = "取消";
-//                 this.bombDocument.appendChild(this.bombYesBtn);
-//                 this.bombDocument.appendChild(this.bombNoBtn);
-//                 if (type === "prompt") {
-//                     this.promptInput = document.createElement('input');
-//                     this.promptInput.className = "prompt_input";
-//                     this.promptInput.type = "text";
-//                     this.promptInput.value = this.dataJ.promptValue;
-//                     this.bombHtml.appendChild(this.promptInput);
-//                 };
-//                 if (type === "textarea") {
-//                     this.promptInput = document.createElement('textarea');
-//                     this.promptInput.className = "textarea_input";
-//                     this.promptInput.value = this.dataJ.textareaValue;
-//                     this.bombHtml.appendChild(this.promptInput);
-//                 };
-//                 this.bombDocument.appendChild(this.bombNoBtn);
-//                 this.bombDocument.appendChild(this.bombYesBtn);
-//             };
-//         };
-
-//         this.bombDocument.appendChild(this.bombClose);
-//         this.bombDocument.appendChild(this.bombHtml);
-//         if (this.dataJ.bombClass) { this.bombDocument.className += " " + this.dataJ.bombClass; }
-
-//         if (this.dataJ.bg) {
-//             this.bombBg = document.createElement('div');
-//             this.bombBg.className = 'bomb_bg';
-//             if(!this.dataJ.initStatus)
-//                 this.bombBg.style.display="none";
-//             this.dataJ.append.appendChild(this.bombBg);
-//         };
-
-//         if(!this.dataJ.initStatus){
-//             this.bombDocument.style.display="none";
-//         }
-
-//         this.dataJ.append.appendChild(this.bombDocument);
-
-
-//         if(!this.dataJ.initStatus){//当前DOM状态
-//             this.disStatus=1; 
-//         }else{
-//             this.disStatus=2;
-//         }
-//     };
-
-//     BombBox.prototype.show = function() {
-//         if(this.disStatus === 1){
-//             this.disStatus=2;
-//             if(this.bombBg)this.bombBg.style.display="block";
-//             this.bombDocument.style.display="block";
-//         }
-//         return this;
-//     }
-
-//     BombBox.prototype.html = function(t, fn) { this.bombHtml.innerHTML = t; if (fn) fn(this);return this; }
-
-//     BombBox.prototype.yesFn = function(This, type) {
-//         var _value = true;
-//         this.close(this.dataJ.form);
-//         if (type === "prompt" || type === "textarea") {
-//             this.promptInput.value ? _value = this.promptInput.value : _value = true;
-//         } else if (type === "confirm") {
-//             _value = true
-//         };
-//         this.dataJ.yesBtnFn(_value)
-//         return this;
-//     };
-
-//     BombBox.prototype.center = function(fn) {
-//         this.bombDocument.style.top = 0;// 'top:0;left:0; bottom:0; right:0; margin:auto;'
-//         this.bombDocument.style.left = 0;
-//         this.bombDocument.style.bottom = 0;
-//         this.bombDocument.style.right = 0;
-//         this.bombDocument.style.margin = 'auto';
-//         if (fn) { fn() }
-//         return this;
-//     };
-
-//     //关闭
-//     BombBox.prototype.close = function(type) {
-//         if( type === 'stick' ){
-//             this.bombBg.style.display="none";
-//             this.bombDocument.style.display="none";
-//             this.disStatus=1;
-//         }else{
-//             this.dataJ.append.removeChild(this.bombDocument);
-//             if (this.dataJ.bg == true) { this.dataJ.append.removeChild(this.bombBg); }
-//             this.disStatus=0; 
-//         }
-
-//         this.dataJ.closeFn(this)
-//         return this;
-//     };
-//     window.BombBox = BombBox;
-
-// }()
-// if (typeof(module) !== 'undefined') {
-//     module.exports = window.BombBox;
-// } else if (typeof define === 'function' && define.amd) {
-//     define([], function() {
-//         'use strict';
-//         return window.BombBox;
-//     });
-// }
